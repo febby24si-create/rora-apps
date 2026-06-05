a4ebb3b778cd634d294a1110f1eba4dc0d2521de
febby24si-create <febby24si@mahasiswa.pcr.ac.id>
2026-06-05 09:12:16 +0700

    PERTEMUAN 12: onBoard Screen sama rest API

--- app/src/main/java/com/example/andesiaapps/Home/HomeFragment.kt
+++ app/src/main/java/com/example/andesiaapps/Home/HomeFragment.kt
@@ -38,6 +38,7 @@ class HomeFragment : Fragment() {
             loadCatFact()
         }
 
+
         // Tombol ke FifthActivity
         binding.btnToFifth.setOnClickListener {
             val intent = Intent(requireContext(), FifthActivity::class.java)
